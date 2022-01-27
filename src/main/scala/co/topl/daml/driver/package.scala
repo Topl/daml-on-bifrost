35e86f9ec0dc0b789b9797be0e3843ef2b519bcd
tuxman <n.edmonds@topl.me>
2022-01-27 15:23:02 -0600

    Change Index type from Int to Long

--- src/main/scala/co/topl/daml/driver/package.scala
+++ src/main/scala/co/topl/daml/driver/package.scala
@@ -2,9 +2,7 @@ package co.topl.daml
 
 package object driver {
 
-  // Bifrost supports heights as `Long`, but for indexing purposes here
-  // we will represent offsets as `Int`
-  type Index = Int
+  type Index = Long
 
   val StartIndex: Index = 0
 }
